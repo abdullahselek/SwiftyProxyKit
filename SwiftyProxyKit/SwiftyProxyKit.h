69e57b0ea27086f1fc64c5b1d654768858f943e6
Abdullah Selek <abdullahselek@gmail.com>
2018-03-11 21:47:29 +0000

    Create empty project.

---
+++ SwiftyProxyKit/SwiftyProxyKit.h
@@ -0,0 +1,19 @@
+//
+//  SwiftyProxyKit.h
+//  SwiftyProxyKit
+//
+//  Created by Abdullah Selek on 11.03.18.
+//  Copyright © 2018 Abdullah Selek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftyProxyKit.
+FOUNDATION_EXPORT double SwiftyProxyKitVersionNumber;
+
+//! Project version string for SwiftyProxyKit.
+FOUNDATION_EXPORT const unsigned char SwiftyProxyKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyProxyKit/PublicHeader.h>
+
+
---
+++ SwiftyProxyKitTests/SwiftyProxyKitTests.swift
@@ -0,0 +1,36 @@
+//
+//  SwiftyProxyKitTests.swift
+//  SwiftyProxyKitTests
+//
+//  Created by Abdullah Selek on 11.03.18.
+//  Copyright © 2018 Abdullah Selek. All rights reserved.
+//
+
+import XCTest
+@testable import SwiftyProxyKit
+
+class SwiftyProxyKitTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
