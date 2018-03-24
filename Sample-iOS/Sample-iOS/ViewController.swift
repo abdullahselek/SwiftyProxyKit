8bf76fd9e7dea330891879e5f7b02163004a5c88
Abdullah Selek <abdullahselek@gmail.com>
2018-03-24 15:50:29 +0000

    Add sample codes.

--- Sample-iOS/Sample-iOS/ViewController.swift
+++ Sample-iOS/Sample-iOS/ViewController.swift
@@ -10,8 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let session = URLSession.shared
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        var request = URLRequest(url: URL(string: "http://127.0.0.1:8080/")!)
+        request.httpMethod = "POST"
+        request.httpBody = "Request Body".data(using: String.Encoding.utf8)!
+
+        let task = session.dataTask(with: request, completionHandler: {(data, response, error) in
+            if let error = error {
+                print("Error: \(error.localizedDescription)")
+            } else if let response = response {
+                print("Response: \(response)")
+            }
+        })
+        task.resume()
     }
 
 }
